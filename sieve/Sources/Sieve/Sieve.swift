5c5f465ad904c40b50ead46377ab56356f51ad52
Doğuş Kaynak <doguskaynak@gmail.com>
2021-05-29 02:42:29 +0300

    fixed tests

--- sieve/Sources/Sieve/Sieve.swift
+++ sieve/Sources/Sieve/Sieve.swift
@@ -2,23 +2,23 @@
 
 struct Sieve {
     let primes: [Int]
-    
+
     init(_ limit: Int) {
         var primes = [2]
-        var checkArray: [Int] = [Int]()
-        
+        var checkArray = Set<Int>()
+
         for i in 3...limit where i % 2 != 0 {
             if !checkArray.contains(i) {
                 primes.append(i)
-                
+
                 var factor = i + i
                 while factor <= limit {
-                    checkArray.insert(factor, at: factor)
+                    checkArray.insert(factor)
                     factor += i
                 }
             }
         }
-        
+
         self.primes = primes
     }
 }
--- space-age/Sources/SpaceAge/SpaceAge.swift
+++ space-age/Sources/SpaceAge/SpaceAge.swift
@@ -1,26 +1,56 @@
 //Solution goes in Sources
 
-struct SpaceAge {
-    let seconds: Int!
-    var onMercury, onVenus, onEarth, onMars,
-        onJupiter, onSaturn, onUranus, onNeptune : Double!
+class SpaceAge {
     
-    init(_ age: Int) {
-        self.seconds = age
-
-        onMercury = getAge(scale: 0.2408467)
-        onVenus = getAge(scale: 0.61519726)
-        onEarth = getAge(scale: 1.0)
-        onMars = getAge(scale: 1.8808158)
-        onJupiter = getAge(scale: 11.862615)
-        onSaturn = getAge(scale: 29.447498)
-        onUranus = getAge(scale: 84.016846)
-        onNeptune = getAge(scale: 164.79132)
-    }
-    
-    private func getAge(scale: Double) -> Double {
-        let orbitalPeriod = 315576.0
+    struct orbitalPeriods {
+        static var earth = Double(31557600)
+        static var mercury = earth * 0.2408467
+        static var venus = earth * 0.61519726
+        static var mars = earth * 1.8808158
+        static var jupiter = earth * 11.862615
+        static var saturn = earth * 29.447498
+        static var uranus = earth * 84.016846
+        static var neptune = earth * 164.79132
         
-        return ((Double(self.seconds) / (orbitalPeriod * scale)) / 100.0).rounded()
     }
+    var seconds : Int
+    
+    init(_ seconds:Int) {
+        self.seconds = seconds
+    }
+    
+    
+    var onEarth:Double{
+        return Double(seconds) / orbitalPeriods.earth
+    }
+    
+    var onMercury:Double{
+        return Double(seconds) / orbitalPeriods.mercury
+    }
+    
+    var onVenus:Double{
+        return Double(seconds) / orbitalPeriods.venus
+    }
+    
+    var onMars:Double{
+        return Double(seconds) / orbitalPeriods.mars
+    }
+    
+    var onJupiter:Double{
+        return Double(seconds) / orbitalPeriods.jupiter
+        
+    }
+    var onSaturn:Double{
+        return Double(seconds) / orbitalPeriods.saturn
+    }
+    
+    
+    var onUranus:Double{
+        return Double(seconds) / orbitalPeriods.uranus
+    }
+    
+    var onNeptune:Double{
+        return Double(seconds) / orbitalPeriods.neptune
+    }
+    
 }
