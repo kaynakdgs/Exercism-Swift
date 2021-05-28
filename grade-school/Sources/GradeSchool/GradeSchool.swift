c1271579fb500f1a1a76b33a8c2f720220126dc1
Doğuş Kaynak <doguskaynak@gmail.com>
2021-05-29 02:01:58 +0300

    fixed grade-school test

--- grade-school/Sources/GradeSchool/GradeSchool.swift
+++ grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -4,13 +4,13 @@ struct GradeSchool {
     
     var roster: [Int: [String]] = [:]
     var sortedRoster: [Int: [String]] {
-        return roster
+        return roster.mapValues { $0.sorted() }
     }
     
     mutating func addStudent(_ name: String, grade: Int) {
         if var gradeList = roster[grade] {
             gradeList.append(name)
-            roster[grade] = gradeList.sorted()
+            roster[grade] = gradeList
         } else {
             roster[grade] = [name]
         }
