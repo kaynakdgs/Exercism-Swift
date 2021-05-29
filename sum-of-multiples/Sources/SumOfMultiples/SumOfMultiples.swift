dd50d6719b83885c76229b9b78b130e613926740
Doğuş Kaynak <doguskaynak@gmail.com>
2021-05-29 03:04:06 +0300

    fixed tests

--- sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
+++ sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -2,21 +2,24 @@
 
 struct SumOfMultiples {
     static func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
-        guard limit > 0 else { return 0 }
-        var multiples = Set<Int>()
-
-        for i in 1...(limit - 1) {
+        var result = 0
+        
+        if limit == 0 {
+            return 0
+        }
+        
+        for i in 1..<limit {
             for j in inMultiples {
                 if j == 0 {
-                    multiples.insert(0)
-                } else {
-                    let n = i % j
-                    if n == 0 {
-                        multiples.insert(i)
-                    }
+                    continue
+                }
+                if i % j == 0 {
+                    result += i
+                    break
                 }
             }
         }
-        return multiples.reduce(0, +)
+        
+        return result
     }
 }
